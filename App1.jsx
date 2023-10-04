f2427be68f67c599e57702763c293d9d7da4e5fc
Arti Shekhawat <109526959+heavycoding2@users.noreply.github.com>
2023-10-04 16:34:24 +0530

    react

---
+++ App1.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CardDetailsForm from './CardDetailsForm';
+import './App1.css'; // Import your CSS file here
+
+function App1() {
+  return (
+    <div className="App1">
+      <h1>Card Details Form</h1>
+      <CardDetailsForm />
+    </div>
+  );
+}
+
+export default App1;
